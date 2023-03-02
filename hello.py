bfbdb745f983a1f588a1da195d203d807fa89ede
Julian Tarazona <tarazonacarlosjulian@gmail.com>
2023-03-02 11:56:25 +0100

    First exercises

--- hello.py
+++ hello.py
@@ -28,3 +28,25 @@ print(date.today())
 
 # Para que este código funcione, debe convertir la fecha en una cadena. Para realizar esta conversión, use la función de utilidad str():
 print("Today's date is: " + str(date.today()))
+
+
+# ----------------------------------------------------------------------
+
+# Your first program
+# You will use a Jupyter notebook to create your first program. Your senior officer wants you to create code to perform a couple of utilities. You will start by displaying today's date. Then you will add code to convert parsecs to lightyears.
+
+# This exercise is broken into a series of steps. For each step you will be presented with the goal for the step, followed by an empty cell. Enter your Python into the cell and run it. The solution for each step will follow each cell.
+
+from datetime import date
+print(date.today())
+
+# Build a unit converter
+# Now it's time to turn your attention to the second utility, converting parsecs to lightyears. One parsec is 3.26 lightyears, so you will multiply parsecs by that value to determine lightyears.
+
+# Create a variable named parsecs and set it to 11. Then add the code to perform the appropriate calculation and store the result in a variable named lightyears. Finally print the result on the screen with so it displays a message which resembles the following:
+
+# 11 parsecs is ___ lightyears
+
+parsecs = 11
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
