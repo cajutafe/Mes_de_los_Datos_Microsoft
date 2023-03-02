f6b578949abce480476731e7d1185744f1c0b378
Julian Tarazona <tarazonacarlosjulian@gmail.com>
2023-03-02 13:28:20 +0100

    Exercise with if + else

--- exercises.py
+++ exercises.py
@@ -31,3 +31,18 @@ parsecs_input = input("number of parsecs ")
 parsecs = int(parsecs_input)
 lightyears = parsecs * 3.26
 print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
+
+
+# ----------------------------------------------------------------------
+# Exercise: Use logic to examine an object's size
+# You will start your project by creating the code to determine if a piece of space debris is of a dangerous size. For this exercise we will use an arbitrary size of 5 meters cubed (5m3); anything larger is a potentially dangerous object.
+
+# For this step you will be presented with the goal for the exercise, followed by an empty cell. Enter your Python into the cell and run it. The solution is at the bottom of the exercise.
+
+# In the cell below, add a variable named object_size and set it to 10 to represent 10m3. Then add an if statement to test if object_size is greater than 5. If it is, display a message saying We need to keep an eye on this object. If it's less than 5, display a message saying Object poses no threat.
+
+object_size = 10
+if object_size > 5:
+    print("We need to keep an eye on this object.")
+else:
+    print("Object poses no threat.")
