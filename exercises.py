31577e4e0b60caf7922ab798b2528777d73a46b0
Julian Tarazona <tarazonacarlosjulian@gmail.com>
2023-03-06 11:27:16 +0100

    First exercise with maths

--- exercises.py
+++ exercises.py
@@ -132,3 +132,35 @@ Gravity on {name}: {gravity} m/s2"""
 # With the template created, it's time to use it to display information about the moon! Use the format function on template to use the template and print the information. Set name, planet, and gravity to the appropriate values.
 
 print(template.format(name=name, planet=planet, gravity=gravity))
+
+
+
+# ----------------------------------------------------------------------
+
+
+# Arithmetic operators in Python
+# Python provides common arithmetic operators so you can perform mathematic operations in your code. These include the four core operations of addition, subtraction, multiplication, and division.
+
+# Let's explore how we can create a program that can calculate the distance between two planets. We'll start by using two planet distances: Earth (149,597,870 km) and Jupiter (778,547,200 km).
+
+# This exercise is broken into a series of steps. For each step you will be presented with the goal for the step, followed by an empty cell. Enter your Python into the cell and run it. The solution for each step will follow each cell.
+
+# Note: Remove the commas when you're using the values.
+
+# Create variables to store the distances
+# Start by creating two variables named first_planet and second_planet. Set first_planet to the distance from the sun to Earth, and second_planet for the distance from the sun to Jupiter.
+
+first_planet = 149597870
+second_planet = 778547200
+
+# Display distance between planets
+# You have two variables which store the distance between each planet and a common point: the sun. You can subtract these two values to determine the distance between the planets.
+
+# Start by adding the code to subtract first_planet from second_planet and store the result in a variable named distance_km. Display the value to the screen.
+
+# Then add the code to convert distance_km to miles by dividing it by 1.609344 (the rough difference between miles and kilometers) and store the result in a variable named distance_mi. Display the value to the screen.
+
+distance_km = second_planet - first_planet
+print(distance_km)
+distance_mi = distance_km / 1.609344
+print(distance_mi)
