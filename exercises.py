fc1dc21954283edae676398335615dcc481be31f
Julian Tarazona <tarazonacarlosjulian@gmail.com>
2023-03-06 13:01:04 +0100

    List exersice

--- exercises.py
+++ exercises.py
@@ -198,3 +198,45 @@ second_planet = int(second_planet_input)
 distance_km = first_planet - second_planet
 print(abs(distance_km))
 
+
+
+
+# ----------------------------------------------------------------------
+
+
+# Exercise: Use lists to store planet names
+# Lists allow you to store multiple values in a single variable. In this notebook you'll create a project to display information about the planets.
+
+# This exercise is broken into a series of steps. For each step you will be presented with the goal for the step, followed by an empty cell. Enter your Python into the cell and run it. The solution for each step will follow each cell.
+
+# Add all planets to a list
+# First, create a variable named planets. Add the eight planets (without Pluto) to the list. The planets are:
+
+# Mercury
+# Venus
+# Earth
+# Mars
+# Jupiter
+# Saturn
+# Uranus
+# Neptune
+# Finish by using print to display the list of planets.
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+print(planets)
+
+# Display the number of planets
+# Next, display the total number of planets by using len and print.
+
+planets_len = len(planets)
+print(planets_len)
+
+# or
+print("There are", len(planets), "planets")
+
+# Add a planet to the list
+# Finally, add Pluto to the list that you created. Then display both the number of planets and the last planet in the list.
+
+planets.append("Pluto")
+print("Actually, there are", len(planets), "planets")
+print(planets[-1], "is the last planet")
