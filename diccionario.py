1863e723f84725445da8b378cc22c407684818d9
Julian Tarazona <tarazonacarlosjulian@gmail.com>
2023-03-09 10:59:13 +0100

    keys() and values()

--- diccionario.py
+++ diccionario.py
@@ -63,3 +63,25 @@ rainfall = {
     'november': 4.2,
     'december': 2.1
 }
+
+# Imagine que quiere mostrar la lista de todas las precipitaciones. Puede escribir el nombre de cada mes, pero resultará tedioso.
+
+for key in rainfall.keys():
+    print(f'{key}: {rainfall[key]}cm')
+
+# Determinación de la existencia de una clave en un diccionario
+# Al actualizar un valor en un diccionario, Python sobrescribirá el valor existente o creará uno, si la clave no existe. Si quiere agregar a un valor en lugar de sobrescribirlo, puede comprobar si la clave existe mediante in. Por ejemplo, si quiere agregar un valor a diciembre o crear uno si no existe, puede usar lo siguiente:
+
+if 'december' in rainfall:
+    rainfall['december'] = rainfall['december'] + 1
+else:
+    rainfall['december'] = 1
+print(rainfall['december'])
+
+# Recuperación de todos los valores
+# De forma similar a keys(), values() devuelve la lista de todos los valores de un diccionario sin sus claves correspondientes. values() puede resultar útil cuando se usa la clave con fines de etiquetado, como en el ejemplo anterior, en el que las claves son el nombre del mes. Puede usar para values() determinar el importe total de las precipitaciones:
+
+total_rainfall = 0
+for value in rainfall.values():
+    total_rainfall = total_rainfall + value
+print(f'There was {total_rainfall}cm in the last quarter')
