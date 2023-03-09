532060cf79d7bdb786b048b1ff5a30dfb6f6e51f
Julian Tarazona <tarazonacarlosjulian@gmail.com>
2023-03-09 12:31:27 +0100

    Function exercises with arguments

--- exercises.py
+++ exercises.py
@@ -381,3 +381,38 @@ for moon in moons:
 average = total_moons / total_planets
 
 print(f'Each planet has an average of {average} moons')
+
+
+
+# ----------------------------------------------------------------------
+
+
+# Exercise: Work with arguments in functions
+# Required arguments in functions are used when functions need those arguments to work properly. In this exercise, you'll construct a fuel report that requires information from several fuel locations throughout the rocket ship.
+# Create a report generation function
+# Your spaceship has three tanks: Main, External and Hydrogen. You want to create an app to display the amount of fuel in each tank, and the average amount of fuel between the three tanks. Because you wish to reuse this code in other projects, you want to create a function with the logic.
+
+# Create a function named generate_report. The function will take three parameters named main_tank, external_tank and hydrogen_tank. When run, the function will display output which resembles the following:
+
+# Fuel report:
+#     Main tank: ##
+#     External tank: ##
+#     Hydrogen tank: ##
+
+def generate_report(main_tank, external_tank, hydrogen_tank):
+    output = f"""Fuel Report:
+    Main tank: {main_tank}
+    External tank: {external_tank}
+    Hydrogen tank: {hydrogen_tank}
+    """
+    print(output)
+
+
+# Run the function
+# With the function created, add the code to run the function with the following values:
+
+# Main tank: 80
+# External tank: 70
+# Hydrogen tank: 75
+
+generate_report(80, 70, 75)
